35718f4dad0114ef1ac66874824d6956237d5ac8
kk0walski <karol.kowalski95@gmail.com>
2018-11-10 19:45:08 +0100

    Fixing dashboard nav

--- src/components/Dasboard.jsx
+++ src/components/Dasboard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Nav from './DasnoardNav';
+import { NavLink } from "react-router-dom";
 import { Route, Switch, Redirect } from "react-router-dom";
 import GamesList from "./GamesList"
 import CreateGame from "./CreateGame"
@@ -9,12 +9,36 @@ import Account from "./Account"
 
 export default class Dasboard extends Component {
     render() {
-        const {match} = this.props;
-        console.log("MATCH: ", match);
         return (
             <div class="container-fluid">
                 <div class="row wrapper">
-                    <Nav />
+                    <aside className="col-12 col-sm-3 p-0 bg-light">
+                        <nav className="navbar navbar-expand-sm navbar-light bg-light align-items-start flex-sm-column flex-row">
+                            <p className="navbar-brand"><i className="fa fa-bullseye fa-fw"></i> Dashboard</p>
+                            <p className="navbar-toggler" data-toggle="collapse" data-target=".sidebar">
+                                <span className="navbar-toggler-icon"></span>
+                            </p>
+                            <div className="collapse navbar-collapse sidebar">
+                                <ul className="nav nav-pills flex-column">
+                                    <li className="nav-item">
+                                        <NavLink exact to='/dashboard/saved_games' className="nav-link" activeClassName="active">Saved games</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink exact to='/dashboard/create_game' className="nav-link" activeClassName="active">Create game</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink exact to='/dashboard/issues_lists' className="nav-link" activeClassName="active">Saved lists</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink exact to='/dashboard/create_list' className="nav-link" activeClassName="active">Create List</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink exact to='/dashboard/account' className="nav-link" activeClassName="active">My Account</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </aside>
                     <main class="col bg-faded py-3">
                         <Switch >
                             <Route path='/dashboard/saved_games' component={GamesList} />
@@ -22,7 +46,7 @@ export default class Dasboard extends Component {
                             <Route path="/dashboard/create_list" component={CreateList} />
                             <Route path="/dashboard/issues_lists" component={IssuesLists} />
                             <Route path="/dashboard/account" component={Account} />
-                            <Route path="/dashboard" component={GamesList}/>
+                            <Redirect to="/dashboard/saved_games" />
                         </Switch >
                     </main>
                 </div>
