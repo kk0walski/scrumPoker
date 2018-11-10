08aafd19c5bbaaf231a7792ae033b0470eb3d279
kk0walski <karol.kowalski95@gmail.com>
2018-11-10 19:25:26 +0100

    Dashboard is working properly

--- src/components/Dasboard.jsx
+++ src/components/Dasboard.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import Nav from './DasnoardNav';
-import RoutingDashboard from "./RoutingDashboard"
+import { Route, Switch, Redirect } from "react-router-dom";
+import GamesList from "./GamesList"
+import CreateGame from "./CreateGame"
+import CreateList from "./CreateList"
+import IssuesLists from './IssuesLists'
+import Account from "./Account"
 
 export default class Dasboard extends Component {
     render() {
+        const {match} = this.props;
+        console.log("MATCH: ", match);
         return (
             <div class="container-fluid">
                 <div class="row wrapper">
-                    <Nav/>
+                    <Nav />
                     <main class="col bg-faded py-3">
-                        <RoutingDashboard/>
+                        <Switch >
+                            <Route path='/dashboard/saved_games' component={GamesList} />
+                            <Route path="/dashboard/create_game" component={CreateGame} />
+                            <Route path="/dashboard/create_list" component={CreateList} />
+                            <Route path="/dashboard/issues_lists" component={IssuesLists} />
+                            <Route path="/dashboard/account" component={Account} />
+                            <Route path="/dashboard" component={GamesList}/>
+                        </Switch >
                     </main>
                 </div>
             </div>
--- src/components/DasnoardNav.jsx
+++ src/components/DasnoardNav.jsx
@@ -2,35 +2,35 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 export default class DasnoardNav extends Component {
-  render() {
-    return (
-        <aside class="col-12 col-sm-3 p-0 bg-light">
-        <nav class="navbar navbar-expand-sm navbar-light bg-light align-items-start flex-sm-column flex-row">
-            <p class="navbar-brand"><i class="fa fa-bullseye fa-fw"></i> Dashboard</p>
-            <a href class="navbar-toggler" data-toggle="collapse" data-target=".sidebar">
-                <span class="navbar-toggler-icon"></span>
-            </a>
-            <div class="collapse navbar-collapse sidebar">
-                <ul class="nav nav-pills flex-column">
-                    <li class="nav-item">
-                        <Link to='saved_games' class="nav-link active" href="#">Saved games</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to='create_game' class="nav-link" href="#">Create game</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to='issues_lists' class="nav-link" href="#">Saved lists</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to='create_list' class="nav-link" href="#">Create List</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to='/account' class="nav-link" href="#">My Account</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </aside>
-    )
-  }
+    render() {
+        return (
+            <aside className="col-12 col-sm-3 p-0 bg-light">
+                <nav className="navbar navbar-expand-sm navbar-light bg-light align-items-start flex-sm-column flex-row">
+                    <p className="navbar-brand"><i className="fa fa-bullseye fa-fw"></i> Dashboard</p>
+                    <p className="navbar-toggler" data-toggle="collapse" data-target=".sidebar">
+                        <span className="navbar-toggler-icon"></span>
+                    </p>
+                    <div className="collapse navbar-collapse sidebar">
+                        <ul className="nav nav-pills flex-column">
+                            <li className="nav-item">
+                                <Link to='/dashboard/saved_games' className="nav-link active" href="#">Saved games</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/dashboard/create_game' className="nav-link" href="#">Create game</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/dashboard/issues_lists' className="nav-link" href="#">Saved lists</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/dashboard/create_list' className="nav-link" href="#">Create List</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/dashboard/account' className="nav-link" href="#">My Account</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </aside>
+        )
+    }
 }
