318ae80166d146a90b936ac1126bc3cb27614d04
kk0walski <karol.kowalski95@gmail.com>
2018-11-12 18:55:03 +0100

    refactoring importing

--- src/components/ImportIssues.jsx
+++ src/components/ImportIssues.jsx
@@ -36,17 +36,6 @@ class ImportIssues extends Component {
             pageRangeDisplayed: 10
         }
     }
-
-    componentDidMount() {
-        const { params } = this.props.match;
-        var issues = this.gh.getIssues(params.owner, params.name);
-        issues.listLabels().then(result => {
-            this.setState({
-                myLabels: result.data.map(element => element.name)
-            })
-        })
-    }
-
     componentDidUpdate(prevProps, prevState) {
         if (this.state.organisation !== prevState.organisation ||
             this.state.repository !== prevState.repository) {
@@ -128,12 +117,12 @@ class ImportIssues extends Component {
     }
 
     importData() {
-        const { filteredData, myLabels } = this.state;
+        const { filteredData } = this.state;
         const { owner, name } = this.props.match.params;
         var issues = this.gh.getIssues(owner, name);
         filteredData.forEach((element) => {
-            const newLabels = Array.from(new Set(element.labels.map(label => label.name))).filter(name => !myLabels.includes(name))
-            const importIssue = { body: element.body, title: element.title, labels: newLabels}
+            const newLabels = Array.from(new Set(element.labels.map(label => label.name)))
+            const importIssue = { body: element.body, title: element.title, labels: newLabels }
             issues.createIssue(importIssue).catch(error => {
                 alert("ERROR: ", error);
             })
@@ -143,95 +132,89 @@ class ImportIssues extends Component {
     render() {
         const { partData, labels, filterText, filterLabels, myLabels } = this.state;
         const { match } = this.props;
-        if (myLabels.length < 1 || myLabels === undefined) {
-            return (
-                <p>Loading...</p>
-            )
-        } else {
-            return (
-                <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="organisation">Organisation name</label>
-                            <input type="text" className="form-control" name="organisation" id="orgInput" aria-describedby="orgHelp" placeholder="Enter organisation" />
-                            <small id="orgHelp" className="form-text text-muted">Organisation which own this repository.</small>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="Repository">Repository</label>
-                            <input type="text" className="form-control" name="repository" id="exampleRepository" placeholder="Repository" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="organisation">Labels</label>
-                            <input type="text" className="form-control" name="labels" id="labelsInput" aria-describedby="labelsHelp" placeholder="Enter organisation" />
-                            <small id="labelsHelp" className="form-text text-muted">Labels for filtering data</small>
-                        </div>
-                        <button type="submit" className="btn btn-primary">SEARCH</button>
-                    </form>
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="organisation">Organisation name</label>
+                        <input type="text" className="form-control" name="organisation" id="orgInput" aria-describedby="orgHelp" placeholder="Enter organisation" />
+                        <small id="orgHelp" className="form-text text-muted">Organisation which own this repository.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Repository">Repository</label>
+                        <input type="text" className="form-control" name="repository" id="exampleRepository" placeholder="Repository" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="organisation">Labels</label>
+                        <input type="text" className="form-control" name="labels" id="labelsInput" aria-describedby="labelsHelp" placeholder="Enter organisation" />
+                        <small id="labelsHelp" className="form-text text-muted">Labels for filtering data</small>
+                    </div>
+                    <button type="submit" className="btn btn-primary">SEARCH</button>
+                </form>
 
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mr-auto">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
 
-                                {/*Dropdown menu labels start */}
+                            {/*Dropdown menu labels start */}
 
-                                <li className="nav-item dropdown">
-                                    <p className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        Labels
+                            <li className="nav-item dropdown">
+                                <p className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Labels
                                         </p>
-                                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <input type="text" className="form-control" placeholder="Filter" onChange={this.changeFilter} />
-                                        <div style={{ maxHeight: "200px", overflow: 'auto' }} >
-                                            {labels.filter(label => label.search(filterText) !== -1).map(label => {
-                                                if (filterLabels.includes(label)) {
-                                                    return (
-                                                        <p className={classnames('dropdown-item', 'active')} key={label} onClick={(e) => this.checkLabel(e, label)}>
-                                                            {label}
-                                                        </p>
-                                                    )
-                                                } else {
-                                                    return (
-                                                        <p className={classnames('dropdown-item')} key={label} onClick={(e) => this.checkLabel(e, label)}>
-                                                            {label}
-                                                        </p>
-                                                    )
-                                                }
-                                            })}
-                                        </div>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <input type="text" className="form-control" placeholder="Filter" onChange={this.changeFilter} />
+                                    <div style={{ maxHeight: "200px", overflow: 'auto' }} >
+                                        {labels.filter(label => label.search(filterText) !== -1).map(label => {
+                                            if (filterLabels.includes(label)) {
+                                                return (
+                                                    <p className={classnames('dropdown-item', 'active')} key={label} onClick={(e) => this.checkLabel(e, label)}>
+                                                        {label}
+                                                    </p>
+                                                )
+                                            } else {
+                                                return (
+                                                    <p className={classnames('dropdown-item')} key={label} onClick={(e) => this.checkLabel(e, label)}>
+                                                        {label}
+                                                    </p>
+                                                )
+                                            }
+                                        })}
                                     </div>
-                                </li>
+                                </div>
+                            </li>
 
-                                {/*Dropdown menu labels start */}
+                            {/*Dropdown menu labels start */}
 
-                                <li class="nav-item">
-                                    <button class="btn btn-outline-success" type="button" onClick={this.importData}>Import filtered data</button>
-                                </li>
+                            <li class="nav-item">
+                                <button class="btn btn-outline-success" type="button" onClick={this.importData}>Import filtered data</button>
+                            </li>
 
-                            </ul>
-                        </div>
-                    </nav>
-                    <ul className="list-group">
-                        {partData.map(node => (
-                            <IssueItem issue={node} match={match} key={node.id} />
-                        ))}
-                    </ul>
-                    <hr />
-                    <Pagination
-                        activePage={this.state.activePage}
-                        itemsCountPerPage={this.state.itemsCountPerPage}
-                        totalItemsCount={this.state.totalItemsCount}
-                        pageRangeDisplayed={10}
-                        onChange={this.handlePageChange}
-                        innerClass="pagination justify-content-center"
-                        itemClass="page-item"
-                        linkClass="page-link"
-                    />
+                        </ul>
+                    </div>
+                </nav>
+                <ul className="list-group">
+                    {partData.map(node => (
+                        <IssueItem issue={node} match={match} key={node.id} />
+                    ))}
+                </ul>
+                <hr />
+                <Pagination
+                    activePage={this.state.activePage}
+                    itemsCountPerPage={this.state.itemsCountPerPage}
+                    totalItemsCount={this.state.totalItemsCount}
+                    pageRangeDisplayed={10}
+                    onChange={this.handlePageChange}
+                    innerClass="pagination justify-content-center"
+                    itemClass="page-item"
+                    linkClass="page-link"
+                />
 
-                </div>
-            )
-        }
+            </div>
+        )
     }
 }
 
